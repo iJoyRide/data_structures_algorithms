1e0fe0b7b22094e4d90b31691dbbed3ea1bafab3
iJoyRide <sumfeis3@gmail.com>
2024-03-19 11:09:53 +0800

    last in first out

--- stack/validparenthese.py
+++ stack/validparenthese.py
@@ -1,20 +1,16 @@
 def isValid(s):
     stack = []
 
-    pairs = {
-        '(':')', 
-        '{':'}', 
-        '[':']'
-        
-        }
-    
+    pairs = {"(": ")", "{": "}", "[": "]"}
+
     for bracket in s:
-        if bracket in pairs: #bracket is a key
+        if bracket in pairs:  # bracket is a key
             stack.append(bracket)
         elif len(stack) == 0 or bracket != pairs[stack.pop()]:
             return False
     return len(stack) == 0
-            
+
+
 t = "((({{]})))"
 s = "["
 
