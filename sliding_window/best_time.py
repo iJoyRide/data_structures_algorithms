5722bbd4849e6e79bd0e995088a8cbbfc474204f
iJoyRide <sumfeis3@gmail.com>
2024-03-29 16:19:19 +0800

    sliding window

--- sliding_window/best_time.py
+++ sliding_window/best_time.py
@@ -1,19 +1,17 @@
 def maxProfit(prices): 
+    maxP = 0
+    buy = 0
+    sell = 1
     
-    max_profit = 0
-    i = 0
-    left = i
-    right = i+1
-    
-    while left <= right:
-        if prices[left] >= prices[rught]
-            left += 1
-            right += 1
-        else 
-            
+    while sell < len(prices):
+        if prices[buy] < prices[sell]: # so if buy is 7 and sell is 1, it wont go into the loop
+            profit = prices[sell] - prices[buy]
+            maxP = max(profit, maxP)
+        else:
+            buy = sell
+        sell += 1
+    return maxP
         
-        
-        
-prices = [7,1,5,3,6,4]
+prices = [7,3,5,1,6]
 
 print(maxProfit(prices))
