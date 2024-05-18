7090c68410b1813413271020d03ee3c98b011512
iJoyRide <sumfeis3@gmail.com>
2024-05-19 02:09:44 +0800

    time and space

--- Array/merge_string.py
+++ Array/merge_string.py
@@ -13,13 +13,18 @@ def mergeAlternately(word1, word2):
         # Append the ith character of word1 and word2, ['ap', 'bq', 'cr']
         merged.append(word1[i] + word2[i])
 
-    # Append the remaining the character to the end of the list
+    # Append the remaining the character to the end of the list, ['12345']
     merged.extend(word1[min_len:])
     merged.extend(word2[min_len:])
     
-    # Joins all the elements of the "merged" list into a single string
+    # ['ap', 'bq', 'cr', '12345']
+    
+    # Joins all the elements of the "merged" list into a 
+    # single string with empty string, ['adbecf12345']
     return "".join(merged)
     
 
 print(mergeAlternately("abc" , "def12345"))
 
+# Time: 0(N)
+# Space: 0(N)
