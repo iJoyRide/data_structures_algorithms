1fca1fa01525fb0784c6f8f94fa711d8bc6150ee
iJoyRide <sumfeis3@gmail.com>
2024-05-27 22:20:58 +0800

    feat: Highest Altitude

---
+++ Prefix_Sum/highest_altitude.py
@@ -0,0 +1,13 @@
+def largestAltitude(gain):
+    
+    highest = 0
+    
+    for i in range(len(gain)):
+         
+    
+    return
+
+
+if __name__ == "__main__":
+    gain = [-5,1,5,0,-7]
+    print(largestAltitude(gain))
