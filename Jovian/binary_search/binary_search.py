ea59b6a8bd3197473b48fe4dc57a5db0a71c7d1b
iJoyRide <sumfeis3@gmail.com>
2024-09-28 17:41:57 +0800

    binary

---
+++ Jovian/binary_search/binary_search.py
@@ -0,0 +1,44 @@
+from jovian.pythondsa import evaluate_test_case
+
+tests = [
+    # {"input": {"cards": [13, 11, 10, 7, 4, 3, 1, 0], "query": 7}, "output": 3},
+    # {"input": {"cards": [13, 11, 10, 7, 4, 3, 1, 0], "query": 1}, "output": 6},
+    # {"input": {"cards": [4, 2, 1, -1], "query": 4}, "output": 0},
+    # {"input": {"cards": [3, -1, -9, -127], "query": -127}, "output": 3},
+    # {"input": {"cards": [6], "query": 6}, "output": 0},
+    # {"input": {"cards": [9, 7, 5, 2, -9], "query": 4}, "output": -1},
+    # {"input": {"cards": [], "query": 7}, "output": -1},
+    # {"input": {"cards": [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0], "query": 3},"output": 7,},
+    {"input": {"cards": list(range(10000000, 0, -1)), "query": 2},"output": 9999998,},
+]
+
+#Linear Search 
+def linear_locate_card(cards, query):
+    
+    for position in range(len(cards)):
+        if cards[position] == query:
+            return position
+        position += 1
+    return -1
+
+def binary_locate_card(cards, query):
+    lo = 0
+    hi = len(cards) - 1
+    
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        mid_val = cards[mid]
+        
+        if mid_val == query:
+            return mid
+        elif mid_val < query:
+            hi = mid -1
+        else:
+            lo = mid +1
+    return -1
+        
+
+if __name__ == "__main__":
+    for test in tests:
+        evaluate_test_case(linear_locate_card, test)
+        evaluate_test_case(binary_locate_card, test)
